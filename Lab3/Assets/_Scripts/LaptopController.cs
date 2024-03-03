8b0aec025900b0ce9c7c1b261791bbbb361a9085
m-shah02 <mansoorshah488@gmail.com>
2024-03-03 19:47:13 +0000

    Lab 3 - Setup

---
+++ Lab3/Assets/_Scripts/LaptopController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaptopController : MonoBehaviour 
+{
+
+  [SerializeField]
+	private Text timeText;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float t = Time.timeSinceLevelLoad;
+		int mins = (int)( t / 60 );
+		int rest = (int)(t % 60);
+		timeText.text = string.Format("{0:D2}:{1:D2}", mins, rest);
+	}
+}
