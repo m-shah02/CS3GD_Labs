7f18b1b4d8c842fb3880ea269ac400623ee0a7d2
m-shah02 <mansoorshah488@gmail.com>
2024-03-03 23:36:01 +0000

    Lab 4 start

---
+++ Lab4/Assets/Scripts/CubeController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	Color[] colours;
+	int colourIndex;
+	MeshRenderer renderer;
+
+	void Awake() 
+	{
+		// fill array with colours
+		colours = new Color[5];
+		colours[0] = Color.blue;
+		colours[1] = Color.red;
+		colours[2] = Color.green;
+		colours[3] = Color.yellow;
+		colours[4] = Color.cyan;
+
+		// set index to random colour and update material
+		colourIndex = Random.Range (0, colours.Length - 1);
+		renderer = GetComponent<MeshRenderer> ();
+		UpdateMaterial ();
+	}
+
+	void UpdateMaterial() 
+	{
+		// update the colour of the renderer's material
+		renderer.material.color = colours [colourIndex];
+	}
+	
+	public void ChangeColour() 
+	{
+		// select next colour in the array and then update material
+		colourIndex++;
+		if (colourIndex == colours.Length) colourIndex = 0;
+		UpdateMaterial ();
+	}
+
+}
---
+++ Lab4/Assets/Scripts/CubeManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    float t = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CreationRequested())
+        {
+            CreateCube();
+        }
+        t -= Time.deltaTime;
+        
+        if(t < 0)
+        {
+            CreateCube();
+            t += 0.5f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Physics.gravity *= -1.0f;
+        }
+    }
+
+    private void CreateCube()
+    {
+        GameObject cube = GameObject.Instantiate(cubePrefab, transform.position, transform.rotation);
+        cube.transform.parent = transform;
+        //Rigidbody b = cube.GetComponent<Rigidbody>();
+        //b.AddForce(transform.forward * 20, ForceMode.Impulse);
+        //b.mass = Random.Range(1, 100);
+        //b.drag = Random.Range(0, 10);
+    }
+
+    private bool CreationRequested()
+    {
+        return Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.C);
+    }
+}
