408b3438c7c5b4e43064913add28e713fc58f4b2
m-shah02 <mansoorshah488@gmail.com>
2024-03-04 15:05:57 +0000

    Lab 7 - Setup

---
+++ Lab7/Assets/_Scripts/AgentController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour 
+{
+	public enum AgentState 
+	{
+		Idle = 0,
+		Patrolling,
+		Chasing
+	}
+
+	public AgentState state;
+	public Transform[] waypoints;
+	private NavMeshAgent navMeshAgent;
+	private Animator animController;
+	private int speedHashId;
+
+
+	void Awake()
+	{
+		speedHashId    = Animator.StringToHash ("walkingSpeed");
+		navMeshAgent   = GetComponent<NavMeshAgent>();
+		animController = GetComponent<Animator>();
+
+		if (waypoints.Length == 0) 
+		{
+			Debug.LogError("Error: list of waypoints is empty.");
+		}
+	}
+
+	void Update()
+	{
+		if (state == AgentState.Idle)
+			Idle ();
+		else if (state == AgentState.Patrolling)
+			Patrol ();
+		else
+			Chase ();
+	}
+
+	void Chase ()
+	{
+	}
+
+	void Idle() 
+	{
+		animController.SetFloat (speedHashId, 0.0f);
+	}
+
+	void Patrol()
+	{
+		animController.SetFloat (speedHashId, 1.0f);
+	}
+
+
+}
